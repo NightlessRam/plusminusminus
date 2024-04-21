21be8cf432282c5010cbe74a6e4d9c13b35e2b29
Chris Chen <112526973+ChrisChen1771@users.noreply.github.com>
2024-04-21 06:16:16 -0400

    Update app.py for project-part2-obj2
    Added backend handling for real-time userlist, real-time direct message, real-time global chat

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, make_response, current_app
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 from flask_pymongo import PyMongo
 
 import os
@@ -10,14 +11,17 @@ import bleach
 import hashlib
 import secrets
 import pytz
-from auth import validate_password
 
+from auth import validate_password
 
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/myDatabase')
 # app.secret_key = os.environ.get('SECRET_KEY', 'secretkey123456789')
 mongo = PyMongo(app)
+socketio = SocketIO(app)
+
+users = {}  # Maps usernames to user session IDs
 
 #X-Content-Type-Options: nosniff header
 @app.after_request
@@ -29,19 +33,14 @@ def apply_caching(response):
 @app.route('/')
 
 def index():
-    posts = list(mongo.db.posts.find())  #retrieve all posts from database
-
-    token = request.cookies.get('auth_token')
-    #init username as 'Guest'
-    username = 'Guest'
-    if token:
-        #hash token to match stored hash
-        hash_object = hashlib.sha256(token.encode('utf-8'))
-        token_hash = hash_object.hexdigest()
-        session_record = mongo.db.session.find_one({'token_hash': token_hash})
-        
-        if session_record:
-            username = session_record['username']
+    username = get_username_from_token(request.cookies.get('auth_token'))
+    query = {
+        "$or": [
+            {"messageType": {"$ne": "dm"}},
+            {"$or": [{"sender": username}, {"receiver": username}]}
+        ]
+    }
+    posts = list(mongo.db.posts.find(query).sort("created_at", -1))
     return render_template('index.html', posts=posts, username=username)
 
 @app.route('/register', methods=['POST'])
@@ -52,7 +51,7 @@ def register():
 
     if validate_password(password) == False:
         return 'Password is invalid', 400
-    
+
     if password != password2:
         return 'Passwords do not match', 400
 
@@ -105,35 +104,70 @@ def login():
 @app.route('/logout', methods=['POST'])
 def logout():
     token = request.cookies.get('auth_token')
-    
-    #make sure token is not None before proceeding
-    if token is not None:
-        #hash token to match stored hash
+    username = get_username_from_token(token)
+    if token:
         hash_object = hashlib.sha256(token.encode('utf-8'))
         token_hash = hash_object.hexdigest()
-
         mongo.db.session.delete_one({'token_hash': token_hash})
 
-    #clear auth_token cookie
+    if username in users:
+        del users[username]
+        emit('update user list', list(users.keys()), broadcast=True, namespace='/')
+
     response = make_response(redirect(url_for('index')))
-    response.set_cookie('auth_token', '', max_age=0, httponly=True)
+    response.set_cookie('auth_token', '', expires=0, httponly=True, path='/')
     return response
 
 
 @app.route('/posts', methods=['POST'])
 def create_post():
-    content = bleach.clean(request.form['content'])  # Sanitize input
+    try:
+        content = bleach.clean(request.form['content'])  # Sanitize input
+        messageType = request.form.get('messageType', 'post')  # Default to 'post' if not specified
 
-    #retrieve token from cookie
+        #retrieve token from cookie
+        token = request.cookies.get('auth_token')
+        
+        # Handle the image if it exists
+        image = request.files.get('image')
+        image_filename = None
+        if image and allowed_file(image.filename):
+            image_filename = save_image_to_disk(image)
+        
+        #init username as 'Guest'
+        username = 'Guest'
+        if token:
+            # Hash the token to match the stored hash
+            hash_object = hashlib.sha256(token.encode('utf-8'))
+            token_hash = hash_object.hexdigest()
+            # Find the session in the database using the hashed token
+            session_record = mongo.db.session.find_one({'token_hash': token_hash})
+            
+            if session_record:
+                # If a session is found, retrieve the username from the session
+                username = session_record['username']
+        post = {
+            'username': username,
+            'content': content,
+            'image': image_filename,
+            'created_at': datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d'),
+            'messageType': messageType
+        }
+        # Add likes and dislikes only for posts
+        # if messageType == 'post':
+        #     post.update({'likes': 0, 'dislikes': 0})
+            
+        mongo.db.posts.insert_one(post)
+        # post['_id'] = str(post['_id'])  #convert ObjectId to string for JSON serialization
+        # return jsonify(post)
+
+        return jsonify(success=True)
+    except Exception as e:
+        return jsonify(success=False, message=str(e)), 500
+    
+@socketio.on('send chat message')
+def handle_chat_message(data):
     token = request.cookies.get('auth_token')
-    
-    # Handle the image if it exists
-    image = request.files.get('image')
-    image_filename = None
-    if image and allowed_file(image.filename):
-        image_filename = save_image_to_disk(image)
-    
-    #init username as 'Guest'
     username = 'Guest'
     if token:
         # Hash the token to match the stored hash
@@ -145,19 +179,13 @@ def create_post():
         if session_record:
             # If a session is found, retrieve the username from the session
             username = session_record['username']
+    emit('new content', {'content': data['content'], 'username': username}, broadcast=True)
     post = {
         'username': username,
-        'content': content,
-        'image': image_filename,
+        'content': data['content'],
         'created_at': datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d'),
-        'like': 0,  
-        'dislike': 0  
     }
     mongo.db.posts.insert_one(post)
-    # post['_id'] = str(post['_id'])  #convert ObjectId to string for JSON serialization
-    # return jsonify(post)
-    return redirect(url_for('index'))
-
 
 
 @app.route('/interact', methods=['POST'])
@@ -232,7 +260,80 @@ def save_image_to_disk(file):
     file.save(image_path)
     return filename
 
+@socketio.on('connect')
+def handle_connect():
+    username = get_username_from_token(request.cookies.get('auth_token'))
+    if username and username != 'Guest':
+        users[username] = request.sid  # Map current SocketIO session ID to username
+        join_room(username)  # Join a room for private messaging
+        emit('update user list', list(users.keys()), broadcast=True, include_self=False)
+        print(f"{username} connected and added to the user list.")
+    else:
+        print("Guest connected; not adding to the user list.")
+        
+@socketio.on('disconnect')
+def handle_disconnect():
+    # Retrieve username the same way upon disconnect
+    username = get_username_from_token(request.cookies.get('auth_token'))
+    if username and username in users:
+        del users[username]
+        emit('update user list', list(users.keys()), broadcast=True)
 
+@socketio.on('send dm')
+def handle_send_dm(data):
+    sender = get_username_from_token(request.cookies.get('auth_token'))
+    receiver = data['receiver']
+    message = data['message']
+    messageType = 'dm'  # Specify the message type as 'dm'
+
+    if sender and receiver and receiver in users:
+        # Data to send to the receiver
+        dm_data_to_receiver = {
+            'sender': sender,
+            'content': message,
+            'messageType': messageType,
+            'to_self': False,  # Indicates if this message is to self
+            'receiver': receiver
+        }
+        # Data to echo back to the sender
+        dm_data_to_sender = {
+            'sender': sender,
+            'content': message,
+            'messageType': messageType,
+            'to_self': True,  # This message is to self
+            'receiver': receiver
+        }
+        dm = {
+            'sender': sender,
+            'receiver': receiver,
+            'content': message,
+            'created_at': datetime.now(pytz.timezone('US/Eastern')).strftime('%B %d, %Y at %I:%M:%S %p'),
+            'messageType': messageType
+        }
+        # Save the DM to the posts collection
+        mongo.db.posts.insert_one(dm)
+        
+        emit('receive dm', dm_data_to_receiver, room=users[receiver])
+        emit('receive dm', dm_data_to_sender, room=users[sender])
+
+@socketio.on('request_user_list')
+def handle_request_user_list():
+    emit('update user list', list(users.keys()), broadcast=True)
+    print("User list requested and broadcasted.")
+
+def get_username_from_token(token):
+    username = 'Guest'  # Default username if token is invalid or not present
+    if token:
+        try:
+            hash_object = hashlib.sha256(token.encode('utf-8'))
+            token_hash = hash_object.hexdigest()
+            session_record = mongo.db.session.find_one({'token_hash': token_hash})
+            if session_record:
+                username = session_record['username']
+        except Exception as e:
+            print(f"Error retrieving username from token: {e}")
+    return username
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True, use_reloader=False, allow_unsafe_werkzeug=True)
+    # app.run(host='0.0.0.0', port=8080, debug=True)
